9220bd9b8d17a5937ffc2c4ef60769bd67205bdb
Ediz Mete Kurt <edizmetekurt@gmail.com>
2022-09-29 10:21:12 +0300

    add error variables

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,7 +1,15 @@
+/// Api Url
 const String datasUrl = "https://finfree.app/demo";
-
 const Map<String, String> headers = {
   "Content-Type": "application/json",
   "Accept": "application/json",
   "Authorization": "R29vZCBMdWNr",
 };
+
+/// Errors
+const INVALID_RESPONSE = 100;
+const NO_INTERNET = 101;
+const INVALID_FORMAT = 102;
+const UNKNOWN_ERROR = 103;
+
+const SUCCESS = 200;
