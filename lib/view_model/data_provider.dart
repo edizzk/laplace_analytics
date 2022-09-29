16f635abb8da8b96e63e80aa6b018af648148620
Ediz Mete Kurt <edizmetekurt@gmail.com>
2022-09-29 10:07:56 +0300

    create provider class

---
+++ lib/view_model/data_provider.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:laplace_analytics/model/data_model.dart';
+
+import '../repo/data_repo.dart';
+
+class DataProvider extends ChangeNotifier {
+  DataModel _dataModel = DataModel(the1G: [], the1H: [], the1A: [], the3A: [], the1Y: [], the5Y: []);
+  bool _isLoading = false;
+
+  DataModel get dataModel => _dataModel;
+  bool get isLoading => _isLoading;
+
+  setDataModel(DataModel dataModel) {
+    _dataModel = dataModel;
+  }
+
+  setLoading(bool loading) async {
+    _isLoading = loading;
+    notifyListeners();
+  }
+
+  getData() async {
+    setLoading(true);
+    var response = await DataRepository.getData();
+    setDataModel(response);
+    setLoading(false);
+  }
+}
