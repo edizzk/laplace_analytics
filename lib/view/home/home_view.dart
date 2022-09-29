450ac79bd6654ade94eecbaa8a337b24696c3f8f
Ediz Mete Kurt <edizmetekurt@gmail.com>
2022-09-29 13:00:48 +0300

    add lineChart and dateCards method to widget tree

--- lib/view/home/home_view.dart
+++ lib/view/home/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:laplace_analytics/model/data_model.dart';
 import 'package:laplace_analytics/view/home/date_card.dart';
@@ -19,17 +20,52 @@ class HomeView extends StatelessWidget {
   }
 
   Widget _ui(DataProvider dataProvider) {
-    DataModel dataModel = dataProvider.dataModel;
+    List<List<Date>> dateModel = dataProvider.dateModel;
+    return Padding(
+      padding: const EdgeInsets.only(right: 50, left: 50, bottom: 20, top: 20),
+      child: Column(
+        children: [
+          Card(
+            elevation: 10,
+            shape: RoundedRectangleBorder(
+                side: const BorderSide(color: Colors.black54, width: 0.5),
+                borderRadius: BorderRadius.circular(10)),
+            child: Column(
+              children: [
+                _lineChart(dateModel),
+                _dateCards(dateModel),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _lineChart(List<List<Date>> dateModel) {
+    return LineChart(
+      LineChartData(
+        lineBarsData: [
+          LineChartBarData(
+            //spots: _spotList(dateModel[selectedDate])
+          )
+        ]
+      )
+    );
+  }
+
+  Widget _dateCards(List<List<Date>> dateModel) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        dateCard("1G", dataModel.the1G),
-        dateCard("1H", dataModel.the1H),
-        dateCard("1A", dataModel.the1A),
-        dateCard("3A", dataModel.the3A),
-        dateCard("1Y", dataModel.the1Y),
-        dateCard("5Y", dataModel.the5Y),
+        dateCard("1G"),
+        dateCard("1H"),
+        dateCard("1A"),
+        dateCard("3A"),
+        dateCard("1Y"),
+        dateCard("5Y"),
       ],
     );
   }
+
 }
