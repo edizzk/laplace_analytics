b20e420555aa94f3337b6cc12effad399c3b4374
Ediz Mete Kurt <edizmetekurt@gmail.com>
2022-09-29 10:01:15 +0300

    create api constants

---
+++ lib/utils/constants.dart
@@ -0,0 +1,7 @@
+const String datasUrl = "https://finfree.app/demo";
+
+const Map<String, String> headers = {
+  "Content-Type": "application/json",
+  "Accept": "application/json",
+  "Authorization": "R29vZCBMdWNr",
+};
