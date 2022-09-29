46e072a6767d0e735fffd0f38908df4dc914a92e
Ediz Mete Kurt <edizmetekurt@gmail.com>
2022-09-29 12:18:05 +0300

    cover MaterialApp with provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:laplace_analytics/view_model/data_provider.dart';
+import 'package:provider/provider.dart';
 
 import 'view/home/home_view.dart';
 
@@ -10,13 +12,18 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Laplace Analytics',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => DataProvider())
+      ],
+      child: MaterialApp(
+        title: 'Laplace Analytics',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        debugShowCheckedModeBanner: false,
+        home: const HomeView(title: 'Laplace Analytics'),
       ),
-      debugShowCheckedModeBanner: false,
-      home: const HomeView(title: 'Laplace Analytics'),
     );
   }
 }
