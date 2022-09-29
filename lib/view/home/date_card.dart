d684ba0ce1aeef2a9ff38b252a9652863703548b
Ediz Mete Kurt <edizmetekurt@gmail.com>
2022-09-29 13:18:21 +0300

    make typeString with switch case

--- lib/view/home/date_card.dart
+++ lib/view/home/date_card.dart
@@ -1,6 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:laplace_analytics/view_model/data_provider.dart';
 
-Widget dateCard(String type) {
+Widget dateCard(int type, DataProvider dataProvider) {
+
+  String getString() {
+    switch (type) {
+      case 0:
+        return "1G";
+      case 1:
+        return "1H";
+      case 2:
+        return "1A";
+      case 3:
+        return "3A";
+      case 4:
+        return "1Y";
+      case 5:
+        return "5Y";
+      default:
+        return "";
+    }
+  }
+
+  String typeString = getString();
+
   return Expanded(
     child: Card(
       elevation: 10,
@@ -11,8 +34,8 @@ Widget dateCard(String type) {
       child: Padding(
         padding: const EdgeInsets.all(10),
         child: InkWell(
-          onTap: () {},
-          child: Text(type, textAlign: TextAlign.center),
+          onTap: () => dataProvider.setSelectedDate(0),
+          child: Text(typeString, textAlign: TextAlign.center),
         ),
       ),
     ),
